60ae1146ce2b212fd7cc964d5e21b9aba08b9417
Guillermo Antonio Medel Islas <guillermoantoniomedelislas@hotmail.com>
2025-05-23 17:37:14 -0700

    set updated yaml

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,12 +2,12 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   app: {
-    baseURL: process.env.NUXT_APP_BASE_URL || '/',
+    baseURL: '/',
     head: {
       meta: [
         {
           name: 'Content-Security-Policy',
-          content: "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self'; font-src 'self' https://m3del.github.io; connect-src 'self' ${process.env.POCKETBASE_URL ? process.env.POCKETBASE_URL : ''}; frame-src 'self';"
+          content: "default-src 'self'; script-src 'self'; style-src 'self' 'unsafe-inline'; img-src 'self'; font-src 'self' https://m3del.github.io data:; connect-src 'self' ${process.env.POCKETBASE_URL ? process.env.POCKETBASE_URL : ''}; frame-src 'self';"
         }
       ]
     }
@@ -22,18 +22,10 @@ export default defineNuxtConfig({
     'motion-v/nuxt',
   ],
   shadcn: {
-    /**
-     * Prefix for all the imported component
-     */
     prefix: '',
-    /**
-     * Directory that the component lives in.
-     * @default "./components/ui"
-     */
     componentDir: './components/ui'
   },
   build: {
-    // Needed for Framer Motion
     transpile: ['vueuc']
   },
   runtimeConfig: {
@@ -41,7 +33,6 @@ export default defineNuxtConfig({
       pocketbaseUrl: process.env.POCKETBASE_URL
     }
   },
-  // Target static for GitHub Pages
   ssr: false,
   nitro: {
     preset: 'github-pages'
