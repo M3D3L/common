b56fb92a414f55be60fa75ced336bcef7998cc69
Guillermo Antonio Medel Islas <guillermoantoniomedelislas@hotmail.com>
2025-05-23 17:16:16 -0700

    updated the nuxt base url

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   app: {
-    baseURL: process.env.NUXT_APP_BASE_URL || '/portfolio2025/',
+    baseURL: '/',
   },
   devtools: { enabled: true },
   modules: [
