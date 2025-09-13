a7d6132d74baf1ce7d229705e63ec8bdd528ec62
Guillermo Antonio Medel Islas <guillermoantoniomedelislas@hotmail.com>
2025-09-13 16:22:38 -0700

    fixed data binding for blogs

--- pages/index.vue
+++ pages/index.vue
@@ -16,11 +16,8 @@
         
         <TitleBlock title="Skills" description="Full-stack developer focused on sleek digital experiences — a part-time drone pilot bringing both code and cameras to new heights. This unique combination allows for a diverse approach to problem-solving and project execution." 
          class="container pt-16 pb-8 mx-auto md:pt-24" /> 
-        <SectionsSkills :content="layoutConfig.skillsData" />
-        
-        <!-- <SectionsBlogs :content="posts" /> -->
-
-        <SectionsBlogPage class="my-24" :showPagination="false" :content="posts" />
+        <SectionsSkills :content="layoutConfig.skillsData" />-->
+        <SectionsBlogPage v-if="posts?.items" class="my-24" :showPagination="false" :content="posts?.items" />
     </div>
 </template>
 
@@ -41,7 +38,7 @@ const posts = ref([]);
 
 const fetchPosts = async () => {
   try {
-    const result = await pbUtils.fetchCollection('posts', 1, 6, '', '-created', '', ['content']);
+    const result = await pbUtils.fetchCollection('posts', 1, 6, '', '-created', '');
     return result;
   } catch (error) {
     console.error('Error fetching posts:', error);
