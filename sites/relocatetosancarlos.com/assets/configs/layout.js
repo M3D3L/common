12c932d45e151f383ae86f0c3582bb33fd948ef6
gmedel <gmedel@alm.com>
2026-01-03 18:49:45 -0700

    updated layout logic

--- sites/relocatetosancarlos.com/assets/configs/layout.js
+++ sites/relocatetosancarlos.com/assets/configs/layout.js
@@ -2,26 +2,44 @@ import * as layoutSp from "./layoutSp";
 import * as layoutEn from "./layoutEn";
 import { computed } from "vue";
 
-const resolveLayout = () => {
-  const { $isSpanishDomain } = useNuxtApp();
-  return $isSpanishDomain ? layoutSp : layoutEn;
-};
+/**
+ * Logic to determine the current active layout
+ */
+const currentLayout = computed(() => {
+  try {
+    const { $isSpanishDomain } = useNuxtApp();
+    return !$isSpanishDomain ? layoutSp : layoutEn;
+  } catch (e) {
+    return layoutEn;
+  }
+});
 
-// Remove 'as any' and the type ': string'
-const pick = (key) => computed(() => resolveLayout()[key]);
+/**
+ * Automatically creates a reactive mapping for all layout keys.
+ */
+const layout = new Proxy(
+  {},
+  {
+    get(_, prop) {
+      return computed(() => currentLayout.value[prop]);
+    },
+  }
+);
 
-export const seoDefaults = pick("seoDefaults");
-export const contactInfo = pick("contactInfo");
-export const heroSection = pick("heroSection");
-export const servicesSection = pick("servicesSection");
-export const socialsSection = pick("socialsSection");
-export const propertiesSection = pick("propertiesSection");
-export const blogSection = pick("blogSection");
-export const siteMap = pick("siteMap");
-export const socials = pick("socials");
-export const realEstateHeroSection = pick("realEstateHeroSection");
-export const categoryConfigs = pick("categoryConfigs");
-export const contactSection = pick("contactSection");
-export const categories = pick("categories");
-export const categoryHeaders = pick("categoryHeaders");
-export const categoriesHeaders = pick("categoriesHeaders");
+export const {
+  seoDefaults,
+  contactInfo,
+  heroSection,
+  servicesSection,
+  socialsSection,
+  propertiesSection,
+  blogSection,
+  siteMap,
+  socials,
+  realEstateHeroSection,
+  categoryConfigs,
+  contactSection,
+  categories,
+  categoryHeaders,
+  categoriesHeaders,
+} = layout;
