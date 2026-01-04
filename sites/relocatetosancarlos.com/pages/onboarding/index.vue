2ffcce4bbb93cac625a50180e886d0bc62c212f4
gmedel <gmedel@alm.com>
2026-01-03 17:53:07 -0700

    removed unused code

--- sites/relocatetosancarlos.com/pages/onboarding/index.vue
+++
@@ -1,9 +0,0 @@
-<template>
-  <div class="container relative content-center min-h-screen py-10 duration-150 ease-in-out transition-color">
-    <TitleBlock class="mb-6"
-      title="Tell Us About Yourself"
-      description="Share your interests and preferences to help us tailor your experience."/>
-
-      <MoleculesUserTypes />
-  </div>
-</template>
