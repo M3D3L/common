544d09251c169a2d1b028f8e4b147d386ffcd8fb
gmedel <gmedel@alm.com>
2026-08-06 21:11:38 -0700

    updated the promo

--- sites/guillermomedel.com/config/menu-pricing.ts
+++ sites/guillermomedel.com/config/menu-pricing.ts
@@ -14,7 +14,7 @@ export const menuPricingConfig: PricingConfig = {
   promos: [
     {
       id: "meal-combo-180",
-      label: "Combo comida completa",
+      label: "Combo comida completa + extra",
       priority: 25,
       display: {
         summary: "2 guisos + 2 Guarniciones + 1 bebida = $180",
